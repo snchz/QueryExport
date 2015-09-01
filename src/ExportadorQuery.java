3ad5f788de38a44679724ddeb38b6a5fb1e4df8e
snchz <ssaanncchheezz@gmail.com>
2015-09-01 23:05:38 +0200

    Añadido soporte exportar a xlsx

--- src/ExportadorQuery.java
+++ src/ExportadorQuery.java
@@ -6,32 +6,44 @@ import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.List;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 import org.apache.commons.io.FileUtils;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.streaming.SXSSFSheet;
+import org.apache.poi.xssf.streaming.SXSSFWorkbook;
+
 import com.opencsv.*;
 
-
 public class ExportadorQuery {
-	private String _fileOutput, _query, _driver, _url, _user, _pass, _compresion, _CONFIG = "Configuracion.config";
-
+	private String _fileOutput, _query, _driver, _url, _user, _pass, _CONFIG = "Configuracion.config";
+	private enum EXTENSION {ZIP, CSV, XLSX, NONE};
+	private EXTENSION _EXTENSION, _COMPRESION;
 	
-
 	public ExportadorQuery(String configFile) {
-		_CONFIG=configFile;
+		_CONFIG = configFile;
 	}
-	
-	public void Ejecutar() throws Exception{
+
+	public void Ejecutar() throws Exception {
 		System.out.println("Leyendo parametros...");
 		LeerParametros();
 		System.out.println("Lanzando query...");
-		ResultSet rs=ObtenerResulsetQuery();
+		ResultSet rs = ObtenerResulsetQuery();
+		
+		
 		System.out.println("Escribiendo fichero...");
-		EscribirCSV(rs);
-		if (_compresion.equals("ZIP")){
+		if(_EXTENSION==EXTENSION.CSV)
+			EscribirCSV(rs);
+		else if (_EXTENSION==EXTENSION.XLSX)
+			EscribirXLSX(rs);
+		
+		
+		if (_COMPRESION==EXTENSION.ZIP) {
 			System.out.println("Comprimiendo fichero...");
 			Comprimir();
 			System.out.println("Borrando fichero origen...");
@@ -40,11 +52,11 @@ public class ExportadorQuery {
 	}
 
 	private void BorrarFicheroSinComprimir() throws Exception {
-		try{
+		try {
 			File fichero = new File(_fileOutput);
 			fichero.delete();
-		}catch(Exception e){
-			throw new Exception("Error al borrar el archivo "+_fileOutput+"\n\tDetalles: "+e.getMessage());
+		} catch (Exception e) {
+			throw new Exception("Error al borrar el archivo " + _fileOutput + "\n\tDetalles: " + e.getMessage());
 		}
 	}
 
@@ -52,9 +64,9 @@ public class ExportadorQuery {
 		try {
 			// input file
 			FileInputStream in = new FileInputStream(_fileOutput);
-			
+
 			// out put file
-			String _fileOutputZip=_fileOutput.substring(0, _fileOutput.lastIndexOf("."))+".zip";
+			String _fileOutputZip = _fileOutput.substring(0, _fileOutput.lastIndexOf(".")) + ".zip";
 			ZipOutputStream out = new ZipOutputStream(new FileOutputStream(_fileOutputZip));
 
 			// name the file inside the zip file
@@ -71,7 +83,7 @@ public class ExportadorQuery {
 			out.close();
 			in.close();
 		} catch (IOException e) {
-			throw new Exception("Error al Comprimir el archivo "+_fileOutput+"\n\tDetalles: "+e.getMessage());
+			throw new Exception("Error al Comprimir el archivo " + _fileOutput + "\n\tDetalles: " + e.getMessage());
 		}
 	}
 
@@ -83,51 +95,108 @@ public class ExportadorQuery {
 			_user = lines.get(2);
 			_pass = lines.get(3);
 			_fileOutput = lines.get(4);
+			if (_fileOutput.substring(_fileOutput.lastIndexOf(".")+1,_fileOutput.length()).toUpperCase().trim().equals("CSV"))
+				_EXTENSION=EXTENSION.CSV;
+			else if (_fileOutput.substring(_fileOutput.lastIndexOf(".")+1,_fileOutput.length()).toUpperCase().trim().equals("XLSX"))
+				_EXTENSION=EXTENSION.XLSX;
+			else
+				_EXTENSION=EXTENSION.NONE;
+			System.out.println("Extension "+_EXTENSION);
+			
 			_query = lines.get(5);
-			_compresion=lines.get(6);
+			
+			if(lines.get(6).toUpperCase().trim().equals("ZIP"))
+				_COMPRESION = EXTENSION.ZIP;
+			else
+				_COMPRESION=EXTENSION.NONE;
+			System.out.println("Compresion "+_COMPRESION);
+			
 		} catch (IOException e) {
-			throw new Exception("Error al leer el archivo de parametros "+_CONFIG+"\n\tDetalles: "+e.getMessage());
+			throw new Exception(
+					"Error al leer el archivo de parametros " + _CONFIG + "\n\tDetalles: " + e.getMessage());
 		}
 	}
-	
-	private void EscribirCSV(ResultSet rs) throws Exception{
+
+	private void EscribirCSV(ResultSet rs) throws Exception {
 		CSVWriter wr;
 		try {
 			wr = new CSVWriter(new FileWriter(_fileOutput), ';', CSVWriter.NO_QUOTE_CHARACTER);
 			wr.writeAll(rs, true);
 			wr.flush();
 			wr.close();
-			
+
 			rs.close();
 		} catch (IOException | SQLException e) {
-			throw new Exception("Error al generar CSV "+_fileOutput+"\n\tDetalles: "+e.getMessage());
+			throw new Exception("Error al generar CSV " + _fileOutput + "\n\tDetalles: " + e.getMessage());
 		}
 	}
-	
+
+	private void EscribirXLSX(ResultSet rs) throws Exception {
+		//TODO Fala ver si el close del wb afecta en algo.
+		try {
+
+			SXSSFWorkbook wb = new SXSSFWorkbook();
+			wb.setCompressTempFiles(true);
+
+			SXSSFSheet sh = (SXSSFSheet) wb.createSheet("Hoja1");
+			// keep 100 rows in memory, exceeding rows will be flushed to disk
+			sh.setRandomAccessWindowSize(100);
+			int totalColumnas=rs.getMetaData().getColumnCount();
+			
+			//Cabecera
+			Row heading = sh.createRow(1);
+            ResultSetMetaData rsmd = rs.getMetaData();
+            for(int x = 0; x < totalColumnas; x++) {
+                Cell cell = heading.createCell(x+1);
+                cell.setCellValue(rsmd.getColumnLabel(x+1));
+            }
+			
+			
+            //Datos
+            int rowNumber = 2;
+
+            while(rs.next()) {
+            	Row row = sh.createRow(rowNumber);
+            	for (int y = 0; y < totalColumnas; y++) {
+					Cell cell = row.createCell(y+1);
+					cell.setCellValue(rs.getString(y+1));            		
+            	}
+            	rowNumber++;
+            }
+
+			FileOutputStream out = new FileOutputStream(_fileOutput);
+			wb.write(out);
+			out.close();
+
+			rs.close();
+		} catch (SQLException e) {
+			throw new Exception("Error al generar XLSX " + _fileOutput + "\n\tDetalles: " + e.getMessage());
+		}
+	}
 
 	private ResultSet ObtenerResulsetQuery() throws Exception {
 		Statement stmt;
 		Connection conn;
-		String log="";
+		String log = "";
 		try {
-			log=log+"[Paso 1 de 3]Buscando driver " + _driver + "...\n";
+			log = log + "[Paso 1 de 3]Buscando driver " + _driver + "...\n";
 			Class.forName(_driver);
-			log=log+"Driver " + _driver + " encontrado.\n";
-			log=log+"[Paso 2 de 3]Realizando conexion " + _url + "...\n";
+			log = log + "Driver " + _driver + " encontrado.\n";
+			log = log + "[Paso 2 de 3]Realizando conexion " + _url + "...\n";
 			conn = DriverManager.getConnection(_url, _user, _pass);
-			log=log+"Conexion " + _url + " realizada.\n";
+			log = log + "Conexion " + _url + " realizada.\n";
 			stmt = conn.createStatement();
-			log=log+"[Paso 3 de 3]Lanzando query...\n";
+			log = log + "[Paso 3 de 3]Lanzando query...\n";
 			ResultSet rs = stmt.executeQuery(_query);
-			log=log+"Query obtenida.\n";
-			
-			
+			log = log + "Query obtenida.\n";
+
 			stmt.close();
 			conn.close();
 			return rs;
 		} catch (SQLException | ClassNotFoundException e) {
-			log=log+"ERROR!!!";
-			throw new Exception("Error leer de la base de datos "+_fileOutput+"\n\tLog:\n"+log+"\n\tDetalles: "+e.getMessage());
+			log = log + "ERROR!!!";
+			throw new Exception("Error leer de la base de datos " + _fileOutput + "\n\tLog:\n" + log + "\n\tDetalles: "
+					+ e.getMessage());
 		}
 	}
 
