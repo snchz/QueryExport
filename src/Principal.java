3e30c4d1987c369df8c4d97321a0a5ccf832cd82
snchz <ssaanncchheezz@gmail.com>
2016-09-30 14:38:05 +0200

    Añadido tratamiento de log externo

--- src/Principal.java
+++ src/Principal.java
@@ -14,18 +14,33 @@ public class Principal {
 		Date horaInicio=Calendar.getInstance().getTime();
 		System.out.println("HORA INICIO "+(new SimpleDateFormat("yyyy/MM/dd HH:mm:ss")).format(horaInicio));
 		
-		ExportadorQuery eq=new ExportadorQuery(args[0]);
-		try {
-			eq.Ejecutar();
-		} catch (Exception e) {
-			e.printStackTrace();
+		String resultado="";
+		//args=new String[1];args[0]="TestDMContable.config";
+		
+		if (args.length==0){
+			//TODO Recorrer carpeta de CONFIGURACION
+		}else{
+			ExportadorQuery eq=new ExportadorQuery(args[0]);
+			try {
+				resultado=eq.Ejecutar();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
 		}
 			
 		//Log Fin
 		Date horaFin=Calendar.getInstance().getTime();
 		long minutos=TimeUnit.MINUTES.convert(horaFin.getTime()-horaInicio.getTime(), TimeUnit.MILLISECONDS);
 		System.out.println("HORA FIN "+(new SimpleDateFormat("yyyy/MM/dd HH:mm:ss")).format(horaFin));
-		System.out.println("TIEMPO DE EJECUCION: "+minutos);
+		System.out.println("TIEMPO DE EJECUCION (minutos): "+minutos);
+		//TODO:Escribor en log
+		
+		if (!resultado.equals("IGNORAR")){
+			String lineaLog=(new SimpleDateFormat("yyyy/MM/dd HH:mm:ss")).format(horaInicio)+"\t"+(new SimpleDateFormat("HH:mm:ss")).format(horaFin)+"\t("+String.format("%010d", minutos)+" min)\t"+resultado+"\t"+args[0]+"\n";
+			Fichero log=new Fichero("LOG.txt");
+			log.escribirLineaAlFinal(lineaLog);
+		}
+		
 	}
 
 }
